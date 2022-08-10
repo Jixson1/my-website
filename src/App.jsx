83d10f48d09b3527d27cf06502f12c9d8b14b66e
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2022-08-10 13:50:08 -0500

    Modify color pallete & remove rotating logo stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useEffect } from "react";
 import "./App.css";
 import heart from "./heart.png";
 
 function App() {
-  useEffect(() => {
-    const rotationSpeed = getComputedStyle(document.documentElement).getPropertyValue("--rotation-speed");
-    console.log(rotationSpeed);
-  }, []);
-
-  // modify rotation speed
-  function changeRotationSpeed(speedMod) {
-    const currSpeed = getComputedStyle(document.documentElement).getPropertyValue("--rotation-speed");
-    const parsed = parseFloat(currSpeed);
-    const newSpeed = String(parsed * speedMod + "s");
-    document.documentElement.style.setProperty("--rotation-speed", newSpeed);
-  }
-
-  // size change handler
-  function handleOnSizeChange(e) {
-    document.documentElement.style.setProperty("--size-logo", `${e.currentTarget.value}vmin`);
-  }
 
   // handle image click
-  function onIMGClick() {
-    const currColor = getComputedStyle(document.documentElement).getPropertyValue("--background-color");
-    if (currColor === "rgb(48, 48, 48)")
-      document.documentElement.style.setProperty("--background-color", "rgb(84, 84, 84)");
-    else
-      document.documentElement.style.setProperty("--background-color", "rgb(48, 48, 48)");
+  function onLogoClick() {
+    console.log('Image clicked');
   }
 
   // component html export
@@ -36,20 +14,12 @@ function App() {
       <title>Jackson Buchmeyer</title>
       <div className="App">
         <header className="Top-menu">
-
+          <div onClick={onLogoClick} className="Heart-box">
+            <img src={heart} className="Heart-logo" alt="heart" />
+          </div>
         </header>
         <header className="App-header">
 
-          <p>
-            <input type="range" name="size" min="0" max="100" defaultValue="40" onChange={handleOnSizeChange}></input>
-          </p>
-          <p onClick={onIMGClick}>
-            <img src={heart} className="Heart-logo" alt="heart" />
-          </p>
-          <p>
-            <button class="button button1" onClick={() => changeRotationSpeed(0.5)}>Increase Speed</button>
-            <button class="button button2" onClick={() => changeRotationSpeed(2)}>Decrease Speed</button>
-          </p>
         </header>
       </div>
     </>

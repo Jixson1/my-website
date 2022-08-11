1c3d10f86f681338fd358ff827deb25162afb38d
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2022-08-11 12:07:29 -0500

    Begin implementation of heart dropdown menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,23 +3,38 @@ import heart from "./images/heart.png";
 
 function Menu() {
 
-  // handle image click
-  // function onLogoClick() {
-  //   console.log('Image clicked');
-  // }
-
   function onNameClick() {
     console.log('Name button clicked');
   }
+
+  // handling dropdown menu
+  document.addEventListener('click', e => {
+    const isDropDownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return;
+    
+    let currentDropdown
+    if (isDropDownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove('active');
+    });
+  })
 
   // component html export
   return (
     <>
       <header className="Top-menu">
-        <div className="Dropdown">
-          <button className="Links">
+        <div className="Dropdown" data-dropdown>
+          <button className="Links" data-dropdown-button>
             <img src={heart} className="Heart-logo" alt="heart" />
           </button>
+          <div className="Dropdown-menu">
+            Dropdown Content
+          </div>
         </div>
         <button onClick={onNameClick} className="Name">Jackson Buchmeyer</button>
       </header>

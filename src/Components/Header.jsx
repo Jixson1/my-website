6f2cba3724ac004d0ebbb75f242414beae25ae3e
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2023-06-01 18:40:04 -0500

    begin work again

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 
 function Header() {
   return (
-    <header className="bg-zinc-700">
+    <header className="bg-zinc-700 w-1/2 h-16 fixed left-1/4 rounded-lg">
       <div>
         <Navigation />
       </div>
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -13,9 +13,10 @@ function Home() {
   }, [location])
 
   return (
-    <div>
+    <div className="bg-zinc-700 fixed w-1/2 h-96 bottom-1/3 left-1/4 inline-block rounded-lg">
+      <img src={portrait} alt="Me" className="duration-200 hover:scale-110 hover:ease-in-out hover:bg-button-highlight-color w-1/4 absolute top-1/4"></img>
       <div className="">
-        <h1 className="text-white text-5xl font-bold pl-5">
+        <h1 className="text-white text-xl font-bold pl-5">
           Jackson Buchmeyer
           <p className="pt-10 text-xl text-violet-500">
             Developer
